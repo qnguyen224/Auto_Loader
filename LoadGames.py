3efb9952494d1e61776aaf9cc7928f9a4c845982
Quang-Huy Nguyen <62742219+qnguyen224@users.noreply.github.com>
2021-05-10 13:57:57 -0700

    Update LoadGames.py

--- LoadGames.py
+++ LoadGames.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from time import sleep
+# The following were omitted for privacy reasons.
 VP_WEB = ""
 VP_ACC = ""
 VP_PW = ""
